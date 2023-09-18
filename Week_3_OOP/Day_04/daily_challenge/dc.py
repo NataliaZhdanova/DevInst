2c5ec3a236fa8280385a8c0b4c2b915b8e818df1
NataliaZhdanova <zhdanova.inbox@gmail.com>
2023-09-19 00:28:03 +0300

    Day 04

--- Week_3_OOP/Day_04/daily_challenge/dc.py
+++ Week_3_OOP/Day_04/daily_challenge/dc.py
@@ -14,19 +14,68 @@
 # a method that returns the most common word in the text.
 # a method that returns a list of all the unique words in the text.
 
+class Text :
+    def __init__(self, text) :
+        self.text = text
+
+    def word_frequency(self, word) :
+        words_list = self.text.split()
+        frequency = words_list.count(word)
+        return frequency
+
+    def most_common_word(self) :
+        words_list = self.text.split()
+
+        if not words_list :
+            return "Text is empty."
+
+        word_counts_dict = {}
+        for item in words_list :
+            word_counts_dict[item] = word_counts_dict.get(item, 0) + 1
+
+        most_common = max(word_counts_dict, key=word_counts_dict.get)
+        return most_common
+
+    def unique_words(self) :
+        words_list = self.text.split()
+        unique_words = set(words_list)
+        return list(unique_words)
+    
+    @classmethod
+    def from_file(cls, filename) :
+        with open(filename, "r") as file:
+            file_text = file.read()
+        return cls(file_text)
+
+# text = "A good book would sometimes cost as much as a good house."
+# text_analyzer = Text(text)
+
+
+# word_freq = "good"
+# frequency = text_analyzer.word_frequency(word_freq)
+# print(f"The word '{word_freq}' appears {frequency} times in the text.")
+
+# common_word = text_analyzer.most_common_word()
+# print(f"The most common word in the text is '{common_word}'.")
+
+# unique_words_list = text_analyzer.unique_words()
+# print("Unique words in the text are:", unique_words_list)
+
+
 
 # Part II
 # Then, we will analyze a text coming from an external text file. Download the_stranger.txt file.
-
 # Implement a classmethod that returns a Text instance but with a text file:
-
 #     >>> Text.from_file('the_stranger.txt')
 # Hint: You need to open and read the text from the text file.
-
-
 # Now, use the provided the_stranger.txt file and try using the class you created above.
 
+# text_inst = Text.from_file("C:/Users/User/OneDrive/Documents/DevInst/Week_3_OOP/Day_04/daily_challenge/the_stranger.txt")
+# common_word = text_inst.most_common_word()
+# print(f"The most common word in the text is '{common_word}'.")
 
+# unique_words_list = text_inst.unique_words()
+# print("Unique words in the text are:", unique_words_list)
 
 # Bonus:
 # Create a class called TextModification that inherits from Text.
@@ -35,4 +84,41 @@
 # a method that returns the text without any punctuation.
 # a method that returns the text without any english stop-words (check out what this is !!).
 # a method that returns the text without any special characters.
-# Note: Feel free to implement/create any attribute, method or function needed to make this work, be creative :)
+
+import string
+from nltk.corpus import stopwords
+import re
+
+class TextModification(Text):
+    def __init__(self, text):
+        super().__init__(text)
+
+    def remove_punct(self):
+        translator = str.maketrans('', '', string.punctuation)
+        text_no_punct = self.text.translate(translator)
+        return text_no_punct
+
+    def remove_stopwords(self):
+        import nltk
+        nltk.download('stopwords')
+        english_stopwords = set(stopwords.words("english"))     
+
+        words_list = self.text.split()
+
+        filtered_words = [word for word in words_list if word.lower() not in english_stopwords]
+
+        text_no_stopwords = " ".join(filtered_words)
+        return text_no_stopwords
+
+    def remove_spec_char(self):
+        text_no_spec_chars = re.sub(r'[^A-Za-z0-9\s]', '', self.text)
+        return text_no_spec_chars
+
+
+text_mod = TextModification.from_file("C:/Users/User/OneDrive/Documents/DevInst/Week_3_OOP/Day_04/daily_challenge/the_stranger.txt")
+
+# print("Text without punctuation:", text_mod.remove_punct())
+
+print("Text without stopwords:", text_mod.remove_stopwords())
+
+# print("Text without special characters:", text_mod.remove_spec_char())
--- Week_3_OOP/Day_04/xp_exercises/xp_exercises.py
+++ Week_3_OOP/Day_04/xp_exercises/xp_exercises.py
@@ -1,28 +1,56 @@
-# 🌟 Exercise 1 – Random Sentence Generator
+# Exercise 1 – Random Sentence Generator
 # Instructions
 # Description: In this exercise we will create a random sentence generator. We will do this by asking the user how long the sentence should be and then printing the generated sentence.
-
 # Hint : The generated sentences do not have to make sense.
-
-# Download this word list
-
+# Download the word list
 # Save it in your development directory.
 
 # Create a function called get_words_from_file. This function should read the file’s content and return the words as a collection. What is the correct data type to store the words?
 
+import random
+
+def get_words_from_file(file_name) :
+    with open(file_name, "r") as file :
+        words_list = file.read().split()
+    return words_list
+
 # Create another function called get_random_sentence which takes a single parameter called length. The length parameter will be used to determine how many words the sentence should have. The function should:
 # use the words list to get your random words.
 # the amount of words should be the value of the length parameter.
-
 # Take the random words and create a sentence (using a python method), the sentence should be lower case.
 
+
+def get_random_sentence(words_list, length) :
+    if length < 2 or length > 20 :
+        raise ValueError("Sentence length should be between 2 and 20 words.")
+    
+    random_words = random.sample(words_list, length)
+    random_sentence = " ".join(random_words).lower()
+    return random_sentence
+
 # Create a function called main which will:
-
 # Print a message explaining what the program does.
-
 # Ask the user how long they want the sentence to be. Acceptable values are: an integer between 2 and 20. Validate your data and test your validation!
 # If the user inputs incorrect data, print an error message and end the program.
 # If the user inputs correct data, run your code.
+
+def main() :
+    print("This is a random sentence generator!")
+    try :
+        length = int(input("Provide the sentence length (2-20 words): "))
+#        length = 30
+        if 2 <= length <= 20 :
+            words_list = get_words_from_file("C:/Users/User/OneDrive/Documents/DevInst/Week_3_OOP/Day_04/xp_exercises/words.txt")      
+            sentence = get_random_sentence(words_list, length)
+            print(f"Random sentence: {sentence}.")
+        else :
+            print("Sentence length should be between 2 and 20 words.")
+    except ValueError :
+        print("Please, enter a valid number between 2 and 20.")
+
+if __name__ == "__main__" :
+    main()
+
 
 
 # Exercise 2: Working With JSON
@@ -41,8 +69,20 @@ sampleJson = """{
 }"""
 
 
+data_dict = json.loads(sampleJson)
+
+salary = data_dict["company"]["employee"]["payable"]["salary"]
+
+data_dict["company"]["employee"]["birth_date"] = "1979-11-11"
+
+
+with open("changed_data.json", "w") as json_file:
+    json.dump(data_dict, json_file, indent=4)
+
+print("Salary:", salary)
+print("Changed JSON Data:")
+print(json.dumps(data_dict, indent=4))
+
 # Access the nested “salary” key from the JSON-string above.
-
 # Add a key called “birth_date” to the JSON-string at the same level as the “name” key.
-
 # Save the dictionary as JSON to a file.
