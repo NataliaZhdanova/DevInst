074c063c9f4abe14fd8f709d5e074c04dffc26ad
NataliaZhdanova <zhdanova.inbox@gmail.com>
2023-09-27 22:01:02 +0300

    exercises

---
+++ Week_2_Python/Day_03/daily_challenge/dc_gold.py
@@ -0,0 +1,31 @@
+# Instructions
+# In cryptography, a Caesar cipher is one of the simplest and most widely known encryption techniques.
+# It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet.
+
+# For example, with a left shift of 3 –> D would be replaced by A,
+# –> E would become B, and so on.
+
+# The method is named after Julius Caesar, who used it in his private correspondence.
+
+# Create a python program that encrypts and decrypts messages with ceasar cypher.
+# The user enters the program, and then the program asks him if he wants to encrypt or decrypt, and then execute encryption/decryption on a given message and a given shift.
+
+# Check out this tutorial
+
+# Hint:
+
+# for letter in text:
+#     cypher_text += chr(ord(letter) + 3)
+
+
+set1 = {"apple", "banana", "cherry"}  
+set2 = {"google", "microsoft", "apple"} 
+
+  
+set3 = {"google", "apple"}
+set4 = {"ibm", "emc", "google", "amazon", "microsoft", "apple", "facebook"} 
+set3.issubset(set2) 
+set4.issuperset(set2) 
+
+
+# there will be no 'apple' in the combined set
