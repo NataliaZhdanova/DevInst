a2ed78ec3b1d9a569a1f3f38bb38a054188c64dc
NataliaZhdanova <zhdanova.inbox@gmail.com>
2023-09-30 01:43:53 +0300

    gold daily challenge day 03

--- Week_2_Python/Day_03/daily_challenge/dc_gold.py
+++ Week_2_Python/Day_03/daily_challenge/dc_gold.py
@@ -16,3 +16,80 @@
 
 # for letter in text:
 #     cypher_text += chr(ord(letter) + 3)
+
+# user_input = input("Enter a text to encode or decode: ")
+# to_do = input("Do you want to encode or decode it? Type 'encode' or 'decode'... ")
+# shift = int(input("Specify a shift (a positive number): "))
+
+def encode(user_input) :
+    alpha_lower = "abcdefghijklmnopqrstuvwxyz"
+    alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    text_list = user_input.split(" ")
+    encoded_list = []
+    #print(text_list)
+    for item in text_list :
+        word = str()
+        for index in range(0, len(item)) :
+            if item[index] in alpha_upper :
+                char_index = alpha_upper.index(item[index])
+                if char_index + shift < len(alpha_upper):
+                    new_char = alpha_upper[char_index + shift]
+                else: 
+                    new_char = alpha_upper[char_index + shift - len(alpha_upper)]
+                word += new_char
+            elif item[index] in alpha_lower :
+                char_index = alpha_lower.index(item[index])
+                if char_index + shift < len(alpha_lower):
+                    new_char = alpha_lower[char_index + shift]
+                else: 
+                    new_char = alpha_lower[char_index + shift - len(alpha_lower)]
+                word += new_char
+            else :
+                word += item[index]
+        encoded_list.append(word)
+    encoded_text = " ".join(encoded_list)
+    print(f"Your encoded text is: '{encoded_text}'")
+
+def decode(user_input) :
+    alpha_lower = "abcdefghijklmnopqrstuvwxyz"
+    alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    text_list = user_input.split(" ")
+    decoded_list = []
+    #print(text_list)
+    for item in text_list :
+        word = str()
+        for index in range(0, len(item)) :
+            if item[index] in alpha_upper :
+                char_index = alpha_upper.index(item[index])
+                if char_index - shift >= 0:
+                    new_char = alpha_upper[char_index - shift]
+                else: 
+                    new_char = alpha_upper[char_index - shift + len(alpha_upper)]
+                word += new_char
+            elif item[index] in alpha_lower :
+                char_index = alpha_lower.index(item[index])
+                if char_index - shift >= 0:
+                    new_char = alpha_lower[char_index - shift]
+                else: 
+                    new_char = alpha_lower[char_index - shift + len(alpha_lower)]
+                word += new_char
+            else :
+                word += item[index]
+        decoded_list.append(word)
+    decoded_text = " ".join(decoded_list)
+    print(f"Your decoded text is: '{decoded_text}'")
+
+user_input = input("Enter a text to encode or decode: \n")
+to_do = input("Do you want to encode or decode it? Type 'encode' or 'decode': \n")
+shift = int(input("Specify a shift (a positive number): \n"))
+
+if to_do == "encode" :
+    encode(user_input)
+elif to_do == "decode":
+    decode(user_input)
+
+
+# shift = 6
+# user_input = "Mary has a little lamb, white and fluffy, looking like a cloud of fur!"
+# user_input = "Sgxe ngy g rozzrk rgsh, cnozk gtj lralle, ruuqotm roqk g iruaj ul lax!"
+# decode(user_input)
