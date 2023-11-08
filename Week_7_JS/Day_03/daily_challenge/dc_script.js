95b6dc7e693823d704b1e7941de4c5aff1421d3e
NataliaZhdanova <zhdanova.inbox@gmail.com>
2023-11-08 10:15:53 +0200

    Day 3 xp exercises and a daily challenge

--- Week_7_JS/Day_03/daily_challenge/dc_script.js
+++ Week_7_JS/Day_03/daily_challenge/dc_script.js
@@ -1,45 +1,164 @@
 // Instructions
-// Using this object :
+// 1rst Daily Challenge
+// Create two functions. Each function should return a promise.
+// The first function called makeAllCaps(), takes an array of words as an argument
+// If all the words in the array are strings, resolve the promise. The value of the resolved promise is the array of words uppercased.
+// else, reject the promise with a reason.
 
-let client = "Betty";
+function makeAllCaps(words) {
+    return new Promise((resolve, reject) => {
+      if (words.every(word => typeof word === 'string')) {
+        const upperWords = words.map(word => word.toUpperCase());
+        resolve(upperWords);
+      } else {
+        reject("There are non-string elements in this array.");
+      }
+    });
+  }
 
-const groceries = {
-    fruits : ["pear", "apple", "banana"],
-    vegetables: ["tomatoes", "cucumber", "salad"],
-    totalPrice : "35$",
-    other : {
-        paid : false,
-        meansOfPayment : ["cash", "creditCard"]
-    }
+// The second function called sortWords(), takes an array of words uppercased as an argument
+// If the array length is bigger than 4, resolve the promise. The value of the resolved promise is the array of words sorted in alphabetical order.
+// else, reject the promise with a reason.
+
+function sortWords(words) {
+    return new Promise((resolve, reject) => {
+      if (words.length > 4) {
+        const sortedWords = words.sort();
+        resolve(sortedWords);
+      } else {
+        reject("The array length is not bigger than 4.");
+      }
+    });
 }
-// Hint: To do this daily challenge, take a look at today’s lesson Pass By Value & Pass By Reference
 
-// Create an arrow function named displayGroceries, that console.logs the 3 fruits from the groceries object. Use the forEach method.
+//in this example, the catch method is executed
+makeAllCaps([1, "pear", "banana"])
+      .then((arr) => sortWords(arr))
+      .then((result) => console.log(result))
+      .catch(error => console.log(error))
 
-const displayGroceries = (fruits) => fruits.forEach((fruit) => console.log(fruit));
+//in this example, the catch method is executed
+makeAllCaps(["apple", "pear", "banana"])
+      .then((arr) => sortWords(arr))
+      .then((result) => console.log(result))
+      .catch(error => console.log(error))
 
-displayGroceries(groceries.fruits);
+//in this example, you should see in the console, 
+// the array of words uppercased and sorted
+makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
+      .then((arr) => sortWords(arr))
+      .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
+      .catch(error => console.log(error))
 
-// Create another arrow function named cloneGroceries.
+ 
 
-const cloneGroceries = () => {
-    let user = client;
-    console.log(user);
-    let shopping = groceries;
-    console.log(user);
-    console.log(shopping);
+// 2nd Daily Challenge
+const morse = `{
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}`
+// Create three functions. The two first functions should return a promise..
+
+// The first function is named toJs():
+// this function converts the morse json string provided above to a morse javascript object.
+// if the morse javascript object is empty, throw an error (use reject)
+// else return the morse javascript object (use resolve)
+
+function toJs() {
+    return new Promise((resolve, reject) => {
+      const morseObj = JSON.parse(morse);
+      if (Object.keys(morseObj).length === 0) {
+        reject("Morse object is empty.");
+      } else {
+        resolve(morseObj);
+      }
+    });
 }
 
-cloneGroceries();
+// The second function called toMorse(morseJS), takes one argument: the new morse javascript object.
+// This function asks the user for a word or a sentence.
+// if the user entered a character that doesn’t exist in the new morse javascript object, throw an error. (use reject)
+// else return an array with the morse translation of the user’s word.
+// if the user enters the word "Hello", the promise resolves with this value ["....", ".", ".-..", ".-..","---"]
+// if the user entered the word "¡Hola!", the promise rejects because the character "¡" doesn't exist in the morse javascript object
 
-// In the function, create a variable named user that is a copy of the client variable. (Tip : make the user variable equal to the client variable)
-// Change the client variable to “Betty”. Will we also see this modification in the user variable ? Why ?
-// In the function, create a variable named shopping that is equal to the groceries variable.
-// Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ?
-// Change the value of the paid key to false. Will we also see this modification in the shopping object ? Why ?
+function toMorse(morseObj) {
+    return new Promise((resolve, reject) => {
+      const userPrompt = prompt("Enter a word or a sentence:");
+      const inMorse = [];
+      for (let char of userPrompt.toLowerCase()) {
+        if (morseObj[char] === undefined) {
+          reject(`Character '${char}' doesn't exist in the Morse object.`);
+        }
+        inMorse.push(morseObj[char]);
+      }
+      resolve(inMorse);
+    });
+}
 
-// Invoke the cloneGroceries function.
 
-//Answer: Yes, we'll see all these modifications, because both variables in a function point to the same place in memory as the corresponding global variables.
+// The third function called joinWords(morseTranslation), takes one argument: the morse translation array
+// this function joins the morse translation by using line break and display it on the page (ie. On the DOM)
 
+function joinWords(inMorse) {
+    const result = inMorse.join(" ");
+    document.getElementById("result").innerText = result;
+}
 
+// Chain the three functions.
+// Example:
+// "Hello" gives you
+// ....
+// .
+// .-..
+// .-..
+// ---
+  
+// Chaining the three functions
+  toJs()
+    .then(toMorse)
+    .then(joinWords)
+    .catch(error => console.error("Error occurred:", error));
--- Week_7_JS/Day_03/xp_exercises/xp_script.js
+++ Week_7_JS/Day_03/xp_exercises/xp_script.js
@@ -1,228 +1,53 @@
-// 🌟 Exercise 1 : Scope
+// 🌟 Exercise 1 : Comparison
 // Instructions
-// Analyse the code below, and predict what will be the value of a in all the following functions.
-// Write your prediction as comments in a js file. Explain your predictions.
-// // #1
-// function funcOne() {
-//     const a = 5;
-//     if(a > 1) {
-//         a = 3;
-//     }
-//     alert(`inside the funcOne function ${a}`);
-// }
+// Create a function called compareToTen(num) that takes a number as an argument.
+// The function should return a Promise:
+// the promise resolves if the argument is less than or equal to 10
+// the promise rejects if argument is greater than 10.
 
-// funcOne();
-
-// Answer: "a" will be equal to 3, because inner containers have access to variables of outer containers and can change their values.
-
-// // #1.1 - run in the console:
-// funcOne()
-// // #1.2 What will happen if the variable is declared 
-// // with const instead of let ?
-
-// Answer: an error will be output to the console log, because one cannot change the value of a constant variable.
-
-// //#2
-// const a = 0;
-// function funcTwo() {
-//     a = 5;
-// }
-
-// function funcThree() {
-//     alert(`inside the funcThree function ${a}`);
-// }
-
-// Answer: before funcTwo is executed, "a" = 0, after it is executed - "a" = 5, , because inner containers have access to variables of outer containers and can change their values.
-
-// // #2.1 - run in the console:
-// funcThree()
-// funcTwo()
-// funcThree()
-// // #2.2 What will happen if the variable is declared 
-// // with const instead of let ?
-
-// Answer: first, "a" = 0, then an error will be output to the console log, because one cannot change the value of a constant variable.
-
-
-// #3
-// function funcFour() {
-//     window.a = "hello";
-// }
-
-// function funcFive() {
-//     alert(`inside the funcFive function ${a}`);
-// }
-
-// #3.1 - run in the console:
-// funcFour()
-// funcFive()
-
-// Answer: "a" will be equal to "hello", because "window.a" creates a variable in an outer container with regard to both functions.
-
-//#4
-// const a = 1;
-// function funcSix() {
-//     const a = "test";
-//     alert(`inside the funcSix function ${a}`);
-// }
-
-// Answer: "a" will be equal to "test, because this value is in the nearest container."
-
-// #4.1 - run in the console:
-// funcSix()
-// #4.2 What will happen if the variable is declared with const instead of let ?
-
-// Answer: "a" will be equal to "test, because this value is in the nearest container."
-
-// #5
-// const a = 2;
-// if (true) {
-//     const a = 5;
-//     alert(`in the if block ${a}`);
-// }
-// alert(`outside of the if block ${a}`);
-
-// #5.1 - run the code in the console
-// #5.2 What will happen if the variable is declared with const instead of let ?
-
-// Answer: the result will be the same as with "let", because each alert will take a value from a variable belonging to their corresponding container.
-
-
-// 🌟 Exercise 2 : Ternary Operator
-// Instructions
-// Using the code below:
-
-// function winBattle(){
-//     return true;
-// }
-// Transform the winBattle() function to an arrow function.
-// Create a variable called experiencePoints.
-// Assign to this variable, a ternary operator. If winBattle() is true, the experiencePoints variable should be equal to 10, else the variable should be equal to 1.
-// Console.log the experiencePoints variable.
-
-const winBattle = () => true;
-
-const experiencePoints = winBattle() ? 10 : 1;
-console.log(experiencePoints);
-
-
-// 🌟 Exercise 3 : Is It A String ?
-// Instructions
-// Write a JavaScript arrow function that checks whether the value of the argument passed, is a string or not. The function should return true or false
-// Check out the example below to see the expected output
-
-const isString = (string) => typeof string === 'string';
-console.log(isString('hello')); 
-console.log(isString([1, 2, 4, 0]));
-
-// 🌟 Exercise 4 : Find The Sum
-// Instructions
-// Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
-
-const sum = (a, b) => a + b;
-console.log(sum(10, 5));
-
-
-// 🌟 Exercise 5 : Kg And Grams
-// Instructions
-// Create a function that receives a weight in kilograms and returns it in grams. (Hint: 1 kg is 1000gr)
-
-// First, use function declaration and invoke it.
-// Then, use function expression and invoke it.
-// Write in a one line comment, the difference between function declaration and function expression.
-// Finally, use a one line arrow function and invoke it.
-
-// Declaration:
-function toGrams(kilos) {
-    return kilos * 1000;
-}
-console.log(toGrams(5));
-
-// Expression:
-const toGramsNew = function(kilos) {
-    return kilos * 1000;
-};
-console.log(toGramsNew(3));
-
-// Function declarations are hoisted, which means they are moved to the top of the code before execution. Function expressions are not hoisted, and therefore cannot be called before they appear in the code.
-
-// Arrow:
-const toGrams2 = kilos => kilos * 1000;
-console.log(toGrams2(10));
-
-
-// 🌟 Exercise 6 : Fortune Teller
-// Instructions
-// Create a self invoking function that takes 4 arguments: number of children, partner’s name, geographic location, job title.
-// The function should display in the DOM a sentence like "You will be a <job title> in <geographic location>, and married to <partner's name> with <number of children> kids."
-
-(function(numberOfKids, partnerName, location, title) {
-    const sentence = `You will be a ${title} in ${location}, and married to ${partnerName} with ${numberOfKids} kids.`;
-    document.getElementById("output").innerHTML = sentence;
-  })(3, "John", "Tel-Aviv", "Surgeon");
-
-// 🌟 Exercise 7 : Welcome
-// Instructions
-// John has just signed in to your website and you want to welcome him.
-
-// Create a Navbar in your HTML file.
-// In your js file, create a self invoking funtion that takes 1 argument: the name of the user that just signed in.
-// The function should add a div in the nabvar, displaying the name of the user and his profile picture.
-
-(function(welcome) {
-    const navbar = document.getElementById("navbar");
-    const div = document.createElement("div");
-    div.innerHTML = `Welcome, ${welcome}! <img src="https://images.net/profile_pic.png" alt="Profile Picture" width="50" height="50">`;
-    navbar.appendChild(div);
-  })("John");
-
-
-// 🌟 Exercise 8 : Juice Bar
-// Instructions
-// You will use nested functions, to open a new juice bar.
-
-// Part I:
-// The outer function named makeJuice receives 1 argument: the size of the beverage the client wants - small, medium or large.
-
-// The inner function named addIngredients receives 3 ingredients, and displays on the DOM a sentence like 
-// The client wants a <size drink> juice, containing <first ingredient>, <second ingredient>, <third ingredient>".
-
-// Invoke the inner function ONCE inside the outer function. Then invoke the outer function in the global scope.
-
-// function makeJuice(size) {
-//     function addIngredients(in1, in2, in3) {
-//         const sentence = `The client wants a ${size} juice, containing ${in1}, ${in2}, and ${in3}.`;
-//         document.getElementById("juice").innerHTML = sentence;
-//     }
-//     addIngredients("apple", "cucumber", "lime");
-// }
-
-// makeJuice("small");
-
-
-// Part II:
-// In the makeJuice function, create an empty array named ingredients.
-
-// The addIngredients function should now receive 3 ingredients, and push them into the ingredients array.
-
-// Create a new inner function named displayJuice that displays on the DOM a sentence like 
-// The client wants a <size drink> juice, containing <first ingredient>, <second ingredient>, <third ingredient>".
-
-// The client wants 6 ingredients in his juice, therefore, invoke the addIngredients function TWICE. 
-// Then invoke once the displayJuice function. Finally, invoke the makeJuice function in the global scope.
-
-function makeJuice(size) {
-    const ingredients = [];
-    function addIngredients(in1, in2, in3) {
-        ingredients.push(in1, in2, in3);
-    }
-    function displayJuice() {
-        const sentence = `The client wants a ${size} juice, containing ${ingredients.join(", ")}.`;
-        document.getElementById("juice").innerHTML = sentence;
-    }
-    addIngredients("apple", "cucumber", "lime");
-    addIngredients("tomato", "carrot", "selery");
-    displayJuice();
+function compareToTen(num) {
+    let prom = new Promise((resolve, reject) => {
+        if (num <= 10) {
+            resolve(`${num} is less than or equal to 10`);
+        } else {
+            reject(`${num} is greater than 10`);
+        }
+    });
+    prom.then(value => {
+        console.log("OK: " + value );
+    })
+    .catch(error => console.error("Error: " + error));
 }
 
-makeJuice("small");
+compareToTen(9);
+compareToTen(20);
+
+
+// 🌟 Exercise 2 : Promises
+// Instructions
+// Create a promise that resolves itself in 4 seconds and returns a “success” string.
+
+let prom1 = new Promise((resolve, reject) => {
+    setTimeout(resolve => console.log("Success..."), 4000);
+})
+
+// 🌟 Exercise 3 : Resolve & Reject
+// Instructions
+// Use Promise.resolve(value) to create a promise that will resolve itself with a value of 3.
+// Use Promise.reject(error) to create a promise that will reject itself with the string “Boo!”
+
+const promise1 = Promise.resolve(3);
+const promise2 = Promise.reject("Boo!");
+
+// promise1.then(value => {
+//   console.log("Promise resolved with value:", value);
+// }).catch(error => {
+//   console.error("Error occurred:", error);
+// });
+
+// promise2.then(value => {
+//   console.log("Promise resolved with value:", value);
+// }).catch(error => {
+//   console.error("Error occurred:", error);
+// });
+
