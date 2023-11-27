997c6bc553bd8dcd44e740eb4c8914c78b7ebde7
NataliaZhdanova <zhdanova.inbox@gmail.com>
2023-11-27 20:34:18 +0200

    Day 03, daily challenge

---
+++ Week_9_React/Day_03/daily_challenge/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Carousel } from 'react-responsive-carousel';
+import img1 from "./jrfyzvgzvhs1iylduuhj.jpg";
+import img2 from "./liw377az16sxmp9a6ylg.webp";
+import img3 from "./e8fnw35p6zgusq218foj.webp";
+import img4 from "./c1cklkyp6ms02tougufx.webp";
+
+function App() {
+  return (
+      <Carousel>
+          <div class="carousel carousel-slider">
+              <img src={img1} />
+              <p className="legend">Hong Kong</p>
+          </div>
+          <div class="carousel carousel-slider">
+              <img src={img2} />
+              <p className="legend">Las Vegas</p>
+          </div>
+          <div class="carousel carousel-slider">
+              <img src={img3} />
+              <p className="legend">Japan</p>
+          </div>
+          <div class="carousel carousel-slider">
+              <img src={img4} />
+              <p className="legend">Macao</p>
+          </div>
+      </Carousel>
+);
+}
+
+export default App;
