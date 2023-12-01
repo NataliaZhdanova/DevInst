8fbab4a234d6f397568a74175ca142212cbbfcd1
NataliaZhdanova <zhdanova.inbox@gmail.com>
2023-12-01 22:20:05 +0200

    Day 04, daily challenge

--- Week_9_React/Day_04/daily_challenge/src/App.js
+++ Week_9_React/Day_04/daily_challenge/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [languages, setLanguages] = useState([
+    { name: "Php", votes: 0 },
+    { name: "Python", votes: 0 },
+    { name: "JavaScript", votes: 0 },
+    { name: "Java", votes: 0 }
+  ]);
+
+  function increaseVotes(languageIndex) {
+  
+    const updatedLanguages = [...languages];
+    updatedLanguages[languageIndex].votes += 1;
+
+    setLanguages(updatedLanguages);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Voting App</h1>
+      {languages.map((language, index) => (
+        <div key={index}>
+          <p>{language.name}: {language.votes} votes</p>
+          <button onClick={() => increaseVotes(index)}>Vote for {language.name}</button>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
+
+// Instructions : React Voting App
+// Your goal is to create a voting app.
+
+// Steps
+// In the App.js create an array of objects in the state. Each object contains the language and the number of votes per language.
+// Use the below object as a starting point:
+
+// const [languages, setLanguages] = useState([
+//                                             {name: "Php", votes: 0},
+//                                             {name: "Python", votes: 0},
+//                                             {name: "JavaSript", votes: 0},
+//                                             {name: "Java", votes: 0}
+//                                           ])
+// <br>
+// Create a function that increases the state of the votes by one, when you click on a specific language button.
